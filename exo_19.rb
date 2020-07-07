14f642bbdce56879f5b1499fc307d373aa7eb839
itsjayheart <wearethetrueservants@gmail.com>
2020-07-07 23:43:43 +0200

    recommit exo_19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -8,19 +8,22 @@ email_list = [email]
 				email_number = email_number + 1
 				email = "jean.dupont.0" + email_number.to_s + "@email.fr"
 				email_list[0] = email
-			elsif email_number < 10
+			elsif email_number < 9
 				email_number = email_number + 1
 				email = "jean.dupont.0" + email_number.to_s + "@email.fr"
 				email_list << email
-				if email_number % 2 == 0
-				puts email_list[email_number]
-				end
 			else
 				email_number = email_number + 1
 				email = "jean.dupont." + email_number.to_s + "@email.fr"
 				email_list << email
-				if email_number % 2 == 0
-				puts email_list[email_number]
 				end
-			end
 		end
+
+number_countdown = 0
+
+50.times do
+	number_countdown = number_countdown + 1
+		if number_countdown % 2 != 0
+		puts email_list[number_countdown]
+		end
+	end
